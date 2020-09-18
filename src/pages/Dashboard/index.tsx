af6ab7c64807a98817ae000bf7db39ce6d4ff399
Gabriel Porto <gabriel_porto@outlook.com>
2020-09-18 18:54:31 -0300

    add: temp video link

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -32,7 +32,7 @@ interface YoutubeTypes {
 const Dashboard: React.FC = () => {
   const player = useRef<ReactPlayer>(null);
   const [videoFilePath, setVideoFileURL] = useState<string>(
-    'videos/introducao.mp4',
+    'https://r4---sn-bg0eznll.c.drive.google.com/videoplayback?expire=1600478037&ei=FSNlX-SRJIbihga38q-oBw&ip=191.30.108.202&cp=QVRFUkVfV1BRSFhPOnZMZmlPZ1dOcFRtT1NHUXVlUjhMSnB6ZHJKUmtVUmxqZ2lCcExydmdNRW4&id=5f6379f794813ab9&itag=18&source=webdrive&requiressl=yes&mh=Go&mm=32&mn=sn-bg0eznll&ms=su&mv=m&mvi=4&pl=19&ttl=transient&susc=dr&driveid=1HF5hbubkwFnmXlpMxfoFiWriOqPmuqy0&app=explorer&mime=video/mp4&vprv=1&prv=1&dur=83.684&lmt=1600453831001890&mt=1600463480&sparams=expire,ei,ip,cp,id,itag,source,requiressl,ttl,susc,driveid,app,mime,vprv,prv,dur,lmt&sig=AOq0QJ8wRAIgIJy_Au9lw5jOFs3fSYAa6kJG0xdZbciS68yXRMFVRSYCIDV4y-FCsvtYugVxUgzhxkN3uwTEAtDuZS3T4qaGAhSH&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRgIhALqrTOe4_atNMVnEJdlO-L1kHv_rMvnwrQliS-VshIerAiEAxDbNaXcCqq6M0h5Vc7t_k64VRX-4c1MrwUkI0PBlUtM=&cpn=qjVQioGTb7b6wy9n&c=WEB_EMBEDDED_PLAYER&cver=20200918',
   );
 
   const history = useHistory();
